ddab079f0f354d6a5bcd10e497d65867d3ac76f9
gaineryns <gaineryns@gmail.com>
2018-02-17 13:05:33 +0100

    test

--- etc/prod/deploy.php
+++ etc/prod/deploy.php
@@ -8,13 +8,16 @@ return new class extends DefaultDeployer
     {
         return $this->getConfigBuilder()
             // SSH connection string to connect to the remote server (format: user@host-or-IP:port-number)
-            ->server('user@hostname')
+            ->server('steveyongwo.com')
             // the absolute path of the remote server directory where the project is deployed
-            ->deployDir('/var/www/vhosts/symfony-demo')
+            ->deployDir('/var/www/clients/client0/steveyongwo.com/web')
             // the URL of the Git repository where the project code is hosted
-            ->repositoryUrl('https://github.com/symfony/symfony-demo')
+            ->repositoryUrl('git@github.com:gaineryns/testdeploy.git')
             // the repository branch to deploy
             ->repositoryBranch('master')
+            ->useSshAgentForwarding(true)
+
+
         ;
     }
 
