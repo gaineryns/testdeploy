c9f8ed2930764cc3e9ecb892b8cca12fe6ec52f8
gaineryns <gaineryns@gmail.com>
2018-02-18 18:37:09 +0100

    test1

--- src/Controller/FirstController.php
+++ src/Controller/FirstController.php
@@ -14,6 +14,5 @@ class FirstController extends Controller
     public function index()
     {
         // replace this line with your own code!
-        return $this->render('@Maker/demoPage.html.twig', [ 'path' => str_replace($this->getParameter('kernel.project_dir').'/', '', __FILE__) ]);
     }
 }
